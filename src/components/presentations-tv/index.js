1375489e87d5cbf09b95b9064617629df5a6d04d
Natan Iori <natanbaldo11@gmail.com>
2023-08-31 16:32:10 -0300

    alterada ordem das apresentações

--- src/components/presentations-tv/index.js
+++ src/components/presentations-tv/index.js
@@ -17,25 +17,25 @@ const Presentations = () => {
                         <h1>Apresentações na TV</h1>
                         <UlContact>
                             <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/m0cdvUxJy5s" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
-                            </li>
-                            <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/xGaAArJQUGc" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
-                            </li>
-                            <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/DTw44R3NVYo" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
-                            </li>
-                            <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/M_MYFhQSyco" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
-                            </li>
-                            <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/ROfNth_XPk8" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/2zWk1J2TYjA" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
                             </li>
                             <li>
                                 <Iframe width="560" height="315" src="https://www.youtube.com/embed/OO7arCgYH6k" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
                             </li>
                             <li>
-                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/2zWk1J2TYjA" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/ROfNth_XPk8" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                            </li>
+                            <li>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/M_MYFhQSyco" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                            </li>
+                            <li>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/DTw44R3NVYo" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                            </li>
+                            <li>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/xGaAArJQUGc" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
+                            </li>
+                            <li>
+                                <Iframe width="560" height="315" src="https://www.youtube.com/embed/m0cdvUxJy5s" title="YouTube video player" frameborder="0" allowfullscreen></Iframe>
                             </li>
                         </UlContact>
                         <Anchor target='_blank' rel="noopener noreferrer" href='https://www.youtube.com/@PeaodovalleeValentin11'><BsYoutube/>Veja mais em nosso canal do Youtube</Anchor>
